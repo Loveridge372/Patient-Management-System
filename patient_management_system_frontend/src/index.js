e071d363ccc33b96a52f98268eef0725046ca102
Loveridge372 <loveridgezvidzai7@gmail.com>
2025-04-16 20:55:15 +0200

    Refactor index.js and apply MUI theming

--- patient_management_system_frontend/src/index.js
+++ patient_management_system_frontend/src/index.js
@@ -4,13 +4,23 @@ import ReactDOM from 'react-dom/client';
 import App from './MainApp';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+// Create a basic custom theme (you can tweak it later)
 const theme = createTheme({
-  // Customize your theme here
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2', // blue
+    },
+    secondary: {
+      main: '#dc004e', // pink
+    },
+  },
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,3 +32,4 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
